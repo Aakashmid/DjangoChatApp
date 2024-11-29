5636c215d2b9c9afbf0c43ccfe216d6797dcc1cd
Aakash Kumar Jha <aakash13102005@gmail.com>
2024-11-29 22:32:05 +0530

    setup documentation using swagger

--- djChat/djChat/settings.py
+++ djChat/djChat/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # external apps
+    'drf_spectacular',
     'account',
     'server'
 ]
@@ -130,4 +132,17 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL ="account.models.Account" 
+AUTH_USER_MODEL ="account.Account" 
+
+REST_FRAMEWORK = {
+    # YOUR SETTINGS
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
+}
