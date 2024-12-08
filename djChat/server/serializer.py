58a117c9346b8a7497649732f648ab8659afb6f7
Aakash Kumar Jha <aakash13102005@gmail.com>
2024-12-08 23:21:29 +0530

    server data listing filter

--- djChat/server/serializer.py
+++ djChat/server/serializer.py
@@ -1,4 +1,4 @@
-from .models import Category,Server
+from .models import Category,Server , Channel
 from rest_framework import serializers
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -7,7 +7,30 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = '__all__'
     
 
+class ChannelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Channel
+        fields = '__all__'
+
+
 class ServerSerializer(serializers.ModelSerializer):
+    num_members = serializers.SerializerMethodField()
+    channel_server = ChannelSerializer(many=True)
     class Meta:
         model = Server
-        fields = '__all__'
+        exclude = ['member',]
+
+    # def get_num_members(self, obj):
+    #     return obj.member.count()
+
+    def get_num_members(self, obj):
+        if hasattr(obj, 'num_members'):
+            return obj.num_members
+        return None
+    
+    def to_representation(self, instance):
+        data=  super().to_representation(instance)
+        num_members= self.context.get('num_members')
+        if not num_members:
+            data.pop('num_members',None) 
+        return data
--- djChat/server/views.py
+++ djChat/server/views.py
@@ -4,6 +4,7 @@ from rest_framework.viewsets import ViewSet, ModelViewSet
 from rest_framework.exceptions import AuthenticationFailed, ValidationError
 from rest_framework.response import Response
 from .serializer import ServerSerializer , CategorySerializer
+from django.db import models
 # Create your views here.
 
 
@@ -14,6 +15,7 @@ class ServerListViewSet(ViewSet):
         qty = request.query_params.get('qty')
         by_user = request.query_params.get('by_user') == 'true'   
         by_serverid = request.query_params.get('by_serverid')   
+        with_num_members = request.query_params.get('with_num_members') == 'true'
 
         if not request.user.is_authenticated and ( by_serverid or by_user ):
             raise AuthenticationFailed('Authentication Failed')
@@ -24,6 +26,11 @@ class ServerListViewSet(ViewSet):
         if by_user and request.user.is_authenticated:
         # if request.user.is_authenticated:
             self.queryset = self.queryset.filter(member=request.user.id)
+
+
+        #  for getting members number also in data 
+        if with_num_members:
+            self.queryset = self.queryset.annotate(num_members=models.Count('member'))
 
         if qty:
             self.queryset = self.queryset[:int(qty)]   # slicing the queryset 
@@ -37,6 +44,6 @@ class ServerListViewSet(ViewSet):
             except ValueError:
                 raise ValidationError(detail='Server Value Error ')
 
-        serializer = ServerSerializer(self.queryset, many=True)
+        serializer = ServerSerializer(self.queryset, many=True, context={"num_members":with_num_members})
         return Response(serializer.data)
     
